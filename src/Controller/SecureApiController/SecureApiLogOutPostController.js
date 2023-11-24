3fb9376a1b15675f6cd8fad6f35463089a5287bf
Subroto Das <rupashi.bangla.club@gmail.com>
2023-11-25 04:13:32 +0600

    LogOut and Cookie Remove

--- src/Controller/SecureApiController/SecureApiLogOutPostController.js
+++ src/Controller/SecureApiController/SecureApiLogOutPostController.js
@@ -1,6 +1,13 @@
 const secureApiLogOutPostController = async (req, res, next) => {
   try {
-    res.clearCookie("access_token", { maxAge: 0 }).send({ success: true });
+    res
+      .clearCookie("access_token", {
+        maxAge: 0,
+        domain: "https://a11-client-side-search-job.vercel.app/",
+        secure: true, // Ensure the secure attribute matches the original cookie
+        sameSite: "None",
+      })
+      .send({ success: true });
   } catch (error) {
     console.log(err);
   }
